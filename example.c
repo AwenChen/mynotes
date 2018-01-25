5ace9dbc2e0c0a5085ad5a1214020b9d4adc6c98
Zhengwen chen <zhengwen.chen@wz-inc.com>
2018-01-25 20:58:44 +0800

    Add LOG implemented by macro

--- example.c
+++ example.c
@@ -6,6 +6,15 @@
 #define VAR_TO_STRING(x)            #x
 #define VAR_JOINT(x, y)             x##y
 
+/* C99 feature */
+#define LOGD_HEADER                 "DBG"
+#define LOGI_HEADER                 "INFO"
+#define LOGE_HEADER                 "ERR"
+#define _LOG_(...)                  printf(__VA_ARGS__)
+#define LOGD(fmt, ...)              _LOG_("[%s] "fmt, LOGD_HEADER, ##__VA_ARGS__)
+#define LOGI(fmt, ...)              _LOG_("[%s] "fmt, LOGI_HEADER, ##__VA_ARGS__)
+#define LOGE(fmt, ...)              _LOG_("[%s](%s:%d) "fmt, LOGE_HEADER, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
 int main()
 {
     int xy = 100;
@@ -13,5 +22,9 @@ int main()
     MY_PRINT_GCC_EXT("my print in gcc extension\n");
     MY_PRINT_C99("%d\n", VAR_JOINT(x, y));
     MY_PRINT_GCC_EXT("%s\n", VAR_TO_STRING(helloworld));
+
+    LOGD("hello my log\n");
+    LOGI("just a info\n");
+    LOGE("Fatal error\n");
     return 0;
 }
